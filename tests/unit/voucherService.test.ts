6a519b1f19dca0218b9f3aca86e12f4a1f413b97
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-07-28 18:21:15 -0300

    Tests: Apply Discount tests

--- tests/unit/voucherService.test.ts
+++ tests/unit/voucherService.test.ts
@@ -5,7 +5,7 @@ import voucherService from '../../src/services/voucherService';
 const test = {
   id: 1,
   code: "teste",
-  discount: 0.8,
+  discount: 80,
   used: false
 }
 
@@ -27,5 +27,47 @@ describe("Create Voucher", () => {
     await voucherService.createVoucher("", 0)
 
     expect(voucherRepository.createVoucher).toHaveBeenCalled()
+  })
+})
+
+describe("Aplly Voucher", () => {
+  it("should not aplly voucher", async () => {
+    jest.spyOn(voucherRepository, "getVoucherByCode")
+      .mockResolvedValueOnce(undefined)
+
+    await voucherService.applyVoucher("", 0)
+      .catch(err => expect(err.type).toBe("conflict"))
+  })
+
+  it("should aplly discount", async () => {
+    jest.spyOn(voucherRepository, "getVoucherByCode")
+      .mockResolvedValueOnce(test)
+    jest.spyOn(voucherRepository, "useVoucher")
+      .mockResolvedValueOnce(undefined)
+
+    const result = await voucherService.applyVoucher("", 100)
+
+    expect(result).toStrictEqual({
+      amount: 100,
+      discount: 80,
+      finalAmount: 20,
+      applied: true
+    })
+  })
+
+  it("should not aplly discount", async () => {
+    jest.spyOn(voucherRepository, "getVoucherByCode")
+      .mockResolvedValueOnce(test)
+    jest.spyOn(voucherRepository, "useVoucher")
+      .mockResolvedValueOnce(undefined)
+
+    const result = await voucherService.applyVoucher("", 99)
+    
+    expect(result).toStrictEqual({
+      amount: 99,
+      discount: 80,
+      finalAmount: 99,
+      applied: false
+    })
   })
 })
