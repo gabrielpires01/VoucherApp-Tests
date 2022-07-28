a30d8c6e81e140122babcc8662cd77100fe6f14d
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-07-28 18:02:53 -0300

    Test: Create Voucher Tests

--- tests/unit/voucherService.test.ts
+++ tests/unit/voucherService.test.ts
@@ -1,5 +1,31 @@
-describe("voucherService test suite", () => {
-  it("should be always very positive", () => {
-    expect("didi").toBe("didi");
+import {jest} from '@jest/globals';
+import voucherRepository from '../../src/repositories/voucherRepository';
+import voucherService from '../../src/services/voucherService';
+
+const test = {
+  id: 1,
+  code: "teste",
+  discount: 0.8,
+  used: false
+}
+
+describe("Create Voucher", () => {
+  it("should not create voucher", async () => {
+    jest.spyOn(voucherRepository, "getVoucherByCode")
+      .mockResolvedValueOnce(test)
+
+    await voucherService.createVoucher("", 0)
+      .catch(err => expect(err.type).toBe("conflict"))
+  })
+
+  it("should create voucher mock", async () => {
+    jest.spyOn(voucherRepository, "getVoucherByCode")
+      .mockResolvedValueOnce(undefined)
+    jest.spyOn(voucherRepository, "createVoucher")
+      .mockResolvedValueOnce(undefined)
+
+    await voucherService.createVoucher("", 0)
+
+    expect(voucherRepository.createVoucher).toHaveBeenCalled()
   })
 })
